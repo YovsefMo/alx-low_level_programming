d7ea4e475e096547c3ecd5632902329738e14348
YovsefMo <ksa.m157v@gmail.com>
2023-06-16 04:46:48 -0700

    Task 4 done

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -4,20 +4,20 @@
  * main - Entry point
  *
  *
- * Return: Always 0 (Success)
+ * Return: Always 0 (Sucess)
  */
 
 int main(void)
 {
-	char ch;
+	char c;
 
-	for (ch = 'a'; ch <= 'z'; ch++)
+	for (c = 'a' ; c <= 'z' ; c++)
 	{
-		if ((ch == 'q' || ch == 'e') !=1)
-		{	
-			putchar(ch);
+		if ((c == 'q' || c == 'e') != 1)
+		{
+			putchar(c);
 		}
-	
+
 	}
 	putchar('\n');
 	return (0);
