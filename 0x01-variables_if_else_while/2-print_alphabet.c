1b8abddc70e507dbdb4f81601ff62eee37c0da23
YovsefMo <ksa.m157v@gmail.com>
2023-06-16 02:52:44 -0700

    Task 2 done

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - Prints the last digit of a randomly generated number
  *        and whether it is greater than 5, less than 6, or 0.
@@ -10,12 +11,8 @@ int main(void)
     char letter;
 
     for (letter = 'a'; letter <= 'z'; letter++)
-    {
         putchar(letter);
-    }
-
-    putchar('\n');
-
+	putchar('\n');
     return 0;
 }
 
