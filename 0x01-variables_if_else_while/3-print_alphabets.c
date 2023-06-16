02359247431969f0412be01bb4dd53e8e8de84a9
YovsefMo <ksa.m157v@gmail.com>
2023-06-16 04:25:35 -0700

    Task 3 done

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the last digit of a randomly generated number
+ *        and whether it is greater than 5, less than 6, or 0.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char c;
+	
+	for (c = 'a' ; c <= 'z' ; c++)
+	{
+		putchar(c);
+	}
+
+	for (c = 'A' ; c <= 'Z' ; c++)
+        {
+                putchar(c);
+        }
+	putchar('\n');
+	return (0);
+}
