5d07543496c56a56c8c802165584de58c7de3c12
YovsefMo <ksa.m157v@gmail.com>
2023-08-06 05:45:44 +0300

    Task B2

---
+++ 0x01-variables_if_else_while/102-print_comp5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - prints 2 digit combinations
+ * Description: Prints unique 2 digit combinations
+ * Return: 0
+ */
+int main(void)
+{
+	int n, m;
+
+	for (n = 0 ; n <= 98 ; n++)
+	{
+		for (m = n + 1 ; m <= 99 ; m++)
+		{
+			putchar((n / 10) + 48);
+			putchar((n % 10) + 48);
+			putchar(' ');
+			putchar((m / 10) + 48);
+			putchar((m % 10) + 48);
+
+			if (n == 98 && m == 99))
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
