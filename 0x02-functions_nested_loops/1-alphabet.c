4d53185bca2147fde27f3fb10b6713d391bb864b
YovsefMo <ksa.m157v@gmail.com>
2023-08-11 21:16:35 +0300

    Task 1 done

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,12 +2,13 @@
 /**
  * print_alphabet - prints alphabet in lowercase
  * followed by a new line
- * 
+ *
  * Return: 0
  */
 void print_alphabet(void)
 {
 	int i;
+
 	for (i = 'a' ; i <= 'z' ; i++)
 	{
 		_putchar(i);
