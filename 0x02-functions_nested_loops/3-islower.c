7c7bd91abc0351b0c2e1bc98759eed15cdd13c14
YovsefMo <ksa.m157v@gmail.com>
2023-08-11 21:40:21 +0300

    Task 3 done

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _islower - checker
+ * desc: simple checker
+ * Return= 1 if c, else 0
+ */
+int _islower(int c)
+{
+	if (c = 'c')
+		return (1);
+	else
+		return (0);
+}
