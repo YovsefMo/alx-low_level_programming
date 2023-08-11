babcb26f0c60d37f45c8837e08c85a7aa68833bb
YovsefMo <ksa.m157v@gmail.com>
2023-08-11 21:34:02 +0300

    Task 2 done

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,7 +9,7 @@ void print_alphabet_x10(void)
 	int i;
 
 	for (i = 0 ; i < 10 ; i++)
-       {
+	{
 		int j;
 
 		for (j = 'a' ; j <= 'z' ; j++)
@@ -18,5 +18,4 @@ void print_alphabet_x10(void)
 		}
 
 		_putchar(10);
-       }
 }
