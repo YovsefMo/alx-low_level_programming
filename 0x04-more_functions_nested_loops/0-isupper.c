18d5f23c8bf19c4606dc59461d4857a022bff489
YovsefMo <ksa.m157v@gmail.com>
2023-08-12 21:33:52 +0300

    Task 0 done

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * isupper - checks for uppercase
+ * @c: to be checked
+ * Return: 0 or i
+ */
+int _isupper(int c)
+{
+	if (c = 'A' ; c <= 'Z' ; c++)
+		return (1);
+	else
+		return (0);
+}
