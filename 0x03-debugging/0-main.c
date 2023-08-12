d2add82b13f2ff4e506ad0e45e6b477e9f5634dc
YovsefMo <ksa.m157v@gmail.com>
2023-08-12 21:08:45 +0300

    Task 0 done

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - main tester
+ * positive_or_negative - testing
+ * @i: to be tested
+ * Return: i
+ */
+int main(void)
+{
+	int i = 0;
+
+	positive_or_negative(int i);
+
+	return (0);
+}
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,10 @@
+#ifdef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+/**
+ * main.h
+ * Author: YovsefMo
+ * Description: Debuggin'
+ */
+void positive_or_negative(int i);
+#endif /* MAIN_H */
