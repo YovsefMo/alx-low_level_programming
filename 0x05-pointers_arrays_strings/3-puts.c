5a509d4932b3e42af9ea90c63c6b9079a532f2c2
YovsefMo <ksa.m157v@gmail.com>
2023-08-13 21:40:34 +0300

    Task 2 done

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _puts - does the same as puts
+ * @str: target array
+ * Return: void
+ */
+void _puts(char *str)
+{
+	while (*str)
+		_putchar(*str++);
+
+	_putchar('\n');
+}
