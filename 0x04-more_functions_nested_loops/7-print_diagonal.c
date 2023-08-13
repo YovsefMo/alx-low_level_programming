7944a682431cb3f5d66ecdd371b20590ccb36165
YovsefMo <ksa.m157v@gmail.com>
2023-08-13 11:36:16 +0300

    Task 7 done

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_diagonal - prints a dia line
+ * @n: spaces parameter
+ * Return: 0
+ */
+void print_diagonal(int n)
+{
+	int m;
+
+	while(n > 0)
+	{
+		_putchar(' ');
+	}
+
+	_putchar('\\');
+	_putchar('\n');
+}
