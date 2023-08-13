51a1656722e5a46418417bcf523b10bb3d09b8c1
YovsefMo <ksa.m157v@gmail.com>
2023-08-13 11:21:44 +0300

    Task 5 done

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,19 +5,17 @@
  */
 void more_numbers(void)
 {
-	int x, z;
-
-	for (x = 1 ; x < 11 ; x++)
-	{
-		for (z = 0 ; z<= 14 ; z++)
-		{
-			if(z > 9)
-			{
-				_putchar((z / 10) + '0');
-			}
-			_putchar((z % 10) + '0');
-		}
-
-		_putchar('\n');
-	}
+int x, z;
+for (x = 1 ; x < 11 ; x++)
+{
+for (z = 0 ; z<= 14 ; z++)
+{
+if(z > 9)
+{
+_putchar((z / 10) + '0');
+}
+_putchar((z % 10) + '0');
+}
+_putchar('\n');
+}
 }
