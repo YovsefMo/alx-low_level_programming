9745009985b1599223f3e3a77caf1255da12f914
YovsefMo <ksa.m157v@gmail.com>
2023-08-13 12:29:18 +0300

    Task 8 done

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_square - speaks
+ * @size: speaks
+ * Return: void
+ */
+void print_square(int size)
+{
+	int x, y;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	for (x = 0 ; x < size ; x++)
+	{
+		for (y = 0 ; y < size ; y++)
+		{
+			_putchar('#');
+		}
+
+		_putchar('\n');
+		
+	}
+
+}
