5775654cf1e5c7cdb72840df45d4d495281d7161
YovsefMo <ksa.m157v@gmail.com>
2023-08-16 02:52:14 +0300

    Task 1 done

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/** swap_int - swaps 2 ints
+ * @a: pointer to int a
+ * @b: pointer to int b
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{	
+	int c = *a;
+	*a = *b;
+	*b = c;
+}
