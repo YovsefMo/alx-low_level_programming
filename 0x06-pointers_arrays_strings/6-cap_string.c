72472cbf26efa627f03b0685e2c33bc9af5b44c1
YovsefMo <ksa.m157v@gmail.com>
2023-08-17 02:33:54 +0300

    Task 6 done

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * cap_string - Capitalizes all words of a string
+ * cap_string - caps all words
  * @s: targeted string
  * Return: s
  */
 char *cap_string(char *s)
 {
 	int x, z;
-	
+
 	int sprt[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
 	for (x = 0 ; s[x] != '\0' ; x++)
