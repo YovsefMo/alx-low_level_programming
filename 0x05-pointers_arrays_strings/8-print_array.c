52194c184452684640e012cc7f59e6d5d16f166e
YovsefMo <ksa.m157v@gmail.com>
2023-08-16 05:21:02 +0300

    Task 8 done

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - prints array content
+ * @a: targeted array pointer
+ * @n:number of elements
+ */
+void print_array(int *a, int n)
+{
+	int x;
+
+	for (x = 0 ; x < n ; x++)
+	{
+		printf("%d, ", a[x]);
+	}
+
+	_putchar('\n');
+}
