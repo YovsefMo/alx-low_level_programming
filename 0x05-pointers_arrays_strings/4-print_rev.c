d44df585add8c126970527e27eaaf21196180dad
YovsefMo <ksa.m157v@gmail.com>
2023-08-16 03:53:36 +0300

    Task 4 done

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,13 +7,15 @@
 void print_rev(char *s)
 {
 	int i = 0;
-	int x;
+	int x, z;
 
 	while (s[i] == '\0')
 		i++;
 
-	for (x = i - 1 ; x >= s[0] ; x--)
-		_putchar(s[x]);
+	z = i;
+
+	for (x = z - 1 ; z >= 0 ; z--)
+		_putchar(s[z]);
 
 	_putchar('\n');
 
