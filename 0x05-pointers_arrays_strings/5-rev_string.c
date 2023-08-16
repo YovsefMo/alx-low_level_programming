c8736abbdb569d27b1830b048d3854a9de99e2fa
YovsefMo <ksa.m157v@gmail.com>
2023-08-16 04:16:21 +0300

    Task 4 done

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * rev_string - reverses string components
+ * @s: targetted string pointer
+ * Return: 0
+ */
+void rev_string(char *s)
+{
+	int i, x, z;
+	char swp;
+
+	i = 0;
+
+	while (s[i] !='\0' )
+		i++;
+
+	x = i - 1;
+
+	for (z = 0 ; z > i ; z++)
+	{
+		swp = s[z];
+		s[z] = s[x];
+		s[x--] = swp;
+	}
+}
