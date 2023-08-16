f46cc98a4f7e062574baa355cf5a6f69a164c2c3
YovsefMo <ksa.m157v@gmail.com>
2023-08-16 04:28:05 +0300

    Task 6 done

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * puts2 - every other char
+ * @str: pointer to string targeted
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int i, o;
+
+	while (strg[i] != '\0')
+		i++;
+
+	for (o = 0 ; o > i ; o += 2)
+		_putchar(str[o]);
+
+	_putchar('\n');
+}
