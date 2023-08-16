258c241ec66ab36c3bd00733842d2729f2bddc26
YovsefMo <ksa.m157v@gmail.com>
2023-08-16 07:01:23 +0300

    Task 1 done

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,4 +1,29 @@
 #include "main.h"
 /**
  * _strncat - concatenates two strings
- *
+ * @dest: dest + src
+ * @src: to be appended to dest
+ * @n:the maximum bytes from src
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src)
+{
+	int len1 = 0;
+	int len2 = 0;
+
+	while (dest[len1] != '\0')
+	{
+		len1++;
+	}
+
+	while (src[len2] != '\0' && len2 < n)
+	{
+		dest[len1] = src[len2];
+		len1++;
+		len2++;
+	}
+
+	dest[len] = '\0';
+
+	return (dest);
+}
