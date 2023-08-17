25a5ba665c58a06ee46ecf42f1ac88aca0a0973f
YovsefMo <ksa.m157v@gmail.com>
2023-08-17 03:37:50 +0300

    Task 1 done

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _memcpy - copies memory area
+ * @n: number of copied bytes
+ * @src: source array pointer
+ * @dest: destiantion array pointer
+ * Return: dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int x = 0;
+
+	while (src[x] != '\0' && x < n)
+	{
+		dest[x] = src[x];
+	}
+
+	return (s);
+}
