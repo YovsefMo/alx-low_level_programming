f170c4202f2a306189222f7705c91473c86c30e3
YovsefMo <ksa.m157v@gmail.com>
2023-08-17 13:16:08 +0300

    Task 2 done

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,10 +1,11 @@
 #include "main.h"
 /**
  * _strchr - locates a character
- * @s: targeted array pointer
+ * @s - targeted array pointer
  * @c: character to located
  * Return: c or null
  */
+    
 char *_strchr(char *s, char c)
 {
 	while (*s != '\0')
