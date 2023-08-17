41fb9472985098573b778508365dd44d02bf3e80
YovsefMo <ksa.m157v@gmail.com>
2023-08-17 03:32:01 +0300

    Task 0 done

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -3,7 +3,7 @@
  * _memset - fills n memory with b bytes
  * @s: targeted array
  * @b: the byte to be filled with
- * @n number of bytes replaced
+ * @n: number of bytes replaced
  * Return: s
  */
 char *_memset(char *s, char b, unsigned int n)
