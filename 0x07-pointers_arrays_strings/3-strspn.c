f628f2836a504c604f91969ac4183e0d7faf5c36
YovsefMo <ksa.m157v@gmail.com>
2023-08-17 13:13:34 +0300

    Task 3 done

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,7 +9,7 @@ unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int x, y;
 
-	for (x = 0 ; s[x] != '\0' ; s++)
+	for (x = 0 ; s[x] != '\0' ; x++)
 	{
 		for (y = 0 ; accept[y] != '\0' ; y++)
 		{
@@ -19,4 +19,6 @@ unsigned int _strspn(char *s, char *accept)
 			}
 		}
 	}
+
+	return (x);
 }
