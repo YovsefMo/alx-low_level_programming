1a7ee4b9bbf4b28298a0673beaa46c4a431dbc04
YovsefMo <ksa.m157v@gmail.com>
2023-08-18 15:23:37 +0300

    Task 5 done

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -10,9 +10,14 @@ int _sqrt_recursion(int n)
 	if (n < 0)
 		return (-1);
 
-	return ( _sqrt_recursion2(n, 0));
+	return (_sqrt_recursion2(n, 0));
 }
-
+/**
+ * _sqrt_recursion2 - root
+ * @n: input
+ * @i: input (checker)
+ * Return: root or -1
+ */
 int _sqrt_recursion2(int n, int i)
 {
 	if (i * i > n)
