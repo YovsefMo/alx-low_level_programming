da70dd669fb258a890f06b49bb4062e8d73a0c69
YovsefMo <ksa.m157v@gmail.com>
2023-08-18 14:02:16 +0300

    Task 3 done

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,17 +1,17 @@
 #include "main.h"
 /**
- * factorial - calcuated n!
+ * factorial - calcuates n!
  * @n: input
  * Return: factorial
  */
 int factorial(int n)
 {
-	if ((n == 0) || (n == 1))
+	if (n < 0)
 	{
-		return (1);
-	}
-	else if (n < 0)
 		return (-1);
+	}
+	else if ((n == 0) || (n == 1))
+		return (1);
 
-		return (n * factorial(n - 1));
+	return (n * factorial(n - 1));
 }
