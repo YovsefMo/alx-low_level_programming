5e630d8eadd076db57f6b13b8ab046242eaec503
YovsefMo <ksa.m157v@gmail.com>
2023-08-18 11:19:09 +0300

    Task 8 done

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -8,16 +8,17 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int x, sum = 0, sum1 = 0;
+	int x, *y, sum = 0, sum1 = 0;
 
 	for (x = 0 ; x < size ; x++)
 	{
 		sum += a[x * size + x];
 	}
 
+	*y = &x;
 	for (y = size - 1 ; y >= 0 ; y++)
 	{
-		sum1 =+ a[y * size + ( size - y - 1)];
+		sum1 += a[y * size + ( size - y - 1)];
 	}
 	printf("%d, %d\n", sum, sum1);
 }
