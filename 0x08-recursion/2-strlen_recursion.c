b3864c5d355ee3459d1e33d2317d76b98bb64ab5
YovsefMo <ksa.m157v@gmail.com>
2023-08-18 13:45:21 +0300

    Task 2 done

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen_recursion - length of a string
+ * @s: input
+ * Return: length
+ */
+int _strlen_recursion(char *s)
+{
+	int x;
+
+	if (*s)
+	{
+		x++;
+		x += _strlen_recursion(*s + 1);
+	}
+
+	return (x);
+}
