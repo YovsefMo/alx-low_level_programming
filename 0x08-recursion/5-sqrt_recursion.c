fe6dca6127d89801ab52cccb5633f56f22878f43
YovsefMo <ksa.m157v@gmail.com>
2023-08-18 15:17:59 +0300

    Task 5 done

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - root
+ * @n: input
+ * Return: root or -1
+ */
+int _sqrt_recursion(int n)
+{
+	int i = 0;
+
+	if (n < 0)
+		return (-1);
+}
+
+int _sqrt_recursion2(int n, int i)
+{
+	if (i * i > n)
+		return (-1);
+	else if (i * i == n)
+		return (i);
+
+	return _sqrt_recursion2(n, i + 1);
+}
