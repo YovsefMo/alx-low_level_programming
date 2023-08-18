f399a8802169a3bc7777be453ba56e0a52ae325c
YovsefMo <ksa.m157v@gmail.com>
2023-08-18 11:02:39 +0300

    Task 8 done

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * print_diagsums - main diagonal sum
  * @a: 2d arrau input
@@ -7,10 +8,16 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int x, sum = 0;
+	int x, sum = 0, sum1 = 0;
 
 	for (x = 0 ; x < size ; x++)
 	{
-			sum += a[x * size + x];	
+		sum += a[x * size + x];
 	}
+
+	for (y = size - 1 ; y >= 0 ; y++)
+	{
+		sum1 =+ a[y * size + ( size - y - 1)];
+	}
+	printf("%d, %d\n", sum, sum1);
 }
