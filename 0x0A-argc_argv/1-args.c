82e9209b9512b3650e8e55555b0b500f7a299a74
YovsefMo <ksa.m157v@gmail.com>
2023-08-19 16:41:37 +0300

    Task 1 done

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - prints number of args\n
+ * @argc: number of command line args
+ * @argc: array name 
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
