b8425a8452788f3e4a34b408cb005355f2f998be
YovsefMo <ksa.m157v@gmail.com>
2023-08-20 15:03:10 +0300

    Task 2 done

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,50 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * str_concat - cat 2 strings
+ * @s1: first input
+ * @s2: second input
+ * Return: s1 and s2 or NULL
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int x = 0, y = 0, z, q, w = 0, e = 0;
+	char *s3;
+
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	while (s1[x] != '\0')
+		x++;
+
+	while (s2[y] != '\0')
+		y++;
+
+	z = x + y + 1;
+
+	s3 = (char *)malloc(z * sizeof(char));
+
+	if (s3 == NULL)
+	{
+		return (NULL);
+	}
+
+	for (q = 0 ; q < z ; q++)
+		{
+			if (s1[w] != '\0')
+			{
+				s3[q] = s1[w];
+				w++;
+			}
+			if (s3[e] != '\0')
+			{
+				s3[q] = s2[e];
+				e++;
+			}
+		}
+
+	return(s3);
+}
