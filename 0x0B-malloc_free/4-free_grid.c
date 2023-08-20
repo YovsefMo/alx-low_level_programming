5d3b00a7d822170bc1400907d5e29ac7fadc275d
YovsefMo <ksa.m157v@gmail.com>
2023-08-20 15:48:55 +0300

    Task 4 done

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * free_grid - frees alloc_grid
+ * @grid: rows of mtrx
+ * @height: columns
+ * Return: pointer to 2d in array || NULL
+ */
+void free_grid(int **grid, int height)
+{
+	int x;
+	int *p;
+
+	for (x = 0 ; x < height ; x++)
+	{
+		p = grid[x];
+		free(x);
+	}
+	free(grid);
+}
