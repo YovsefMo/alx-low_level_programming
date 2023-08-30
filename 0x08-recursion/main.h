499e6922b0098321550589b337f98f706c7f0248
YovsefMo <ksa.m157v@gmail.com>
2023-08-30 18:41:36 +0300

    Task 6 done

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -12,6 +12,7 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int _sqrt_recursionfr(int n, int m);
 int is_prime_number(int n);
+int fr_prime(int n, int m);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 #endif /* MAIN_H */
