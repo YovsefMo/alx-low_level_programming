af40c284c42907b3d1a087afd79cbcaf67668deb
YovsefMo <ksa.m157v@gmail.com>
2023-08-30 17:49:01 +0300

    Task 5 done

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -4,4 +4,23 @@
  * @n: input
  * Return: square root for n or -1
  */
-
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	return (int _sqrt_recursionfr(n, 0));
+}
+/**
+ * _sqrt _recursionfr - assistant function
+ * @n: input
+ * @m: iterator
+ * Return: the square root of n
+ */
+int _sqrt_recursionfr(int n, int m)
+{
+	if (m * m > n)
+		return (-1);
+	if (m * m == n)
+		return (m);
+	return (_sqrt_recursionfr(n, m + 1));
+}
