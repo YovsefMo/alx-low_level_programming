e072dd944fbaa64771753620f4968f92fa4c8491
YovsefMo <ksa.m157v@gmail.com>
2023-09-01 21:57:18 +0300

    Task B0 done

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -9,14 +9,13 @@
  */
 int main(int argc, char *argv[])
 {
-    int coins[] = {25, 10, 5, 2, 1};
-    int i, cents, minCoins = 0;
-    int numCoins;
-
-    if (argc != 2)
-    {
-        printf("Error\n");
-        return (1);
+	int coins[] = {25, 10, 5, 2, 1};
+	int i, cents, minCoins = 0;
+	int numCoins;
+if (argc != 2)
+{
+printf("Error\n");
+return (1);
     }
 
     cents = atoi(argv[1]);
