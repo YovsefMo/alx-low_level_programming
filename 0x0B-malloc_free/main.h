22348507a344e830d117ac552bec68bcc46c6149
YovsefMo <ksa.m157v@gmail.com>
2023-09-05 22:19:23 +0300

    Task 0 done + ConfigV2

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -4,7 +4,9 @@
  * main.h
  * Author: YovsefMo
  * For: 0x0B-malloc_free
- */
+*/
+#include <stdio.h>
+#include <stdlib.h>
 int _putchar(char c);
 int **alloc_grid(int width, int height);
 char *create_array(unsigned int size, char c);
