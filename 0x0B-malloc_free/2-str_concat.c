69692ef85022c225bc3f8efd4e2297177187cdbe
YovsefMo <ksa.m157v@gmail.com>
2023-09-06 00:47:27 +0300

    Task 2 done

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,15 +12,15 @@ char *str_concat(char *s1, char *s2)
 	
 	while (s1[len1] != '\0')
 	{
-		if (s1 == NULL)
-			s1 = " ";
+		if (s1[len1] == NULL)
+			s1[len1] = "";
 		len1++;
 	}
 
 	while (s2[len2] != '\0')
 	{
-		if (s2 == NULL)
-			s2 = " ";
+		if (s2[len2] == NULL)
+			s2[len2] = "";
 		len2++;
 	}
 	s3 = malloc(sizeof(char) * (len1 + len2 + 1));
