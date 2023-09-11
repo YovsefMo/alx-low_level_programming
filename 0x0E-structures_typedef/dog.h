d48067dea7aad4d48c05ad5069fe0536d46f189c
YovsefMo <ksa.m157v@gmail.com>
2023-09-12 00:07:46 +0300

    Task 4 done + Config 5.0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * struct dog - dog info
@@ -20,6 +21,8 @@ struct dog
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+
 /**
  * dog_t - typedef for dog
  */
