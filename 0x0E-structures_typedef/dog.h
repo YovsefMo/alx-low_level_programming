378d49e1199f0c6bac84feb2b49391e2eca4a60b
YovsefMo <ksa.m157v@gmail.com>
2023-09-11 23:13:28 +0300

    Task 0 done

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * dog - dog info
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ * Description: Basically a dog info
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif /* DOG_H */
