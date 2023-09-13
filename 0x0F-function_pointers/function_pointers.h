15f820043d2e17eaccddc42d6f1769f32a433e7a
YovsefMo <ksa.m157v@gmail.com>
2023-09-13 18:29:30 +0300

    Task 0 done + ConfigV2

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,10 +1,5 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
-/**
- * fuction.pointers.h
- * Author: YovsefMo
- * For: 0x0F-function_pointers
- */
 
 #include <stdio.h>
 #include <stdlib.h>
