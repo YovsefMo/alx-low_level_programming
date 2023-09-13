ace9a284cebc6c4fdfe8b2e724dbffe64e7e6c24
YovsefMo <ksa.m157v@gmail.com>
2023-09-13 18:39:09 +0300

    Task 1 done

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,7 +11,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	int i;
 
-	if (size != NULL && action != NULL)
+	if (array != NULL && action != NULL)
 	{
 		for (i = 0 ; i < (int) size ; i++)
 			action(array[i]);
