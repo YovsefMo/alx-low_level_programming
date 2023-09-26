c5778a2e08ad175a68a141796253bfb872ce78a6
YovsefMo <ksa.m157v@gmail.com>
2023-09-26 12:19:56 +0300

    Task 5 done

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * free_listint - frees a listtint_t list
+ * @head: Const pointer of a listint_t list
+ *
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *current;
+
+	while (head != NULL)
+	{
+		current = *head;
+		*head = (*head)->next;
+		free(current);
+	}
+
+	*head = NULL;
+}
