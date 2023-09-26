a68e21d15bd365cb1654505b0c20d6c054104931
YovsefMo <ksa.m157v@gmail.com>
2023-09-26 12:25:53 +0300

    Task 5 done

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 /**
- * free_listint - frees a listtint_t list
- * @head: Const pointer of a listint_t list
+ * free_listint2 - frees a listtint_t list and sets head to NULL
+ * @head: Pointer to a pointer to the head of a list
  *
  * Return: void
  */
